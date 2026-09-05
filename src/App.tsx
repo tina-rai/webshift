c405fa262291ca976249cd85e6ddf0811457c7cd
Tina <contactina101@gmail.com>
2026-09-05 19:58:12 +0545

    Initialize WebShift project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main className="w-80 p-5">
+      <h1 className="text-2xl font-bold">
+        WebShift
+      </h1>
+
+      <p className="mt-2 text-sm text-gray-500">
+        Understand and transform the web.
+      </p>
+    </main>
+  )
+}
+
+export default App
